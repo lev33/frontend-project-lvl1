dc128a7d2224730ce7952939a586385792a41c26
Lev A <weekend@mail.ru>
2019-04-08 09:28:27 +0300

    add greeting

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+  const actual = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + actual + '!');
+};
+
+export default greeting;
+
