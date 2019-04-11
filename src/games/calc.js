666f9181f272b4a0b38cdbd03ef99ddc1de087ac
Lev A <weekend@mail.ru>
2019-04-11 09:12:56 +0300

    delete eval from calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,16 +1,24 @@
 import { cons } from 'hexlet-pairs';
-import { startGame } from './funcs';
-
-const getRandom = (a, b) => Math.floor(a + Math.random() * (b - a));
+import { startGame, getRandom } from './funcs';
 
 const oper = ['+', '-', '*'];
+
+const getResult = (operation, num1, num2) => {
+  switch (operation) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    default:
+  }
+  return 'error operation';
+};
 
 const calc = () => {
   const num1 = getRandom(0, 100);
   const num2 = getRandom(0, 100);
   const currentOper = oper[getRandom(0, oper.length)];
   const question = `${num1} ${currentOper} ${num2}`;
-  const rightAnswer = eval(question);
+  const rightAnswer = getResult(currentOper, num1, num2);
   return cons(question, String(rightAnswer));
 };
 
